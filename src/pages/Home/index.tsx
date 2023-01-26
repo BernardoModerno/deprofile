8b2c8124a61293ab7804f0d80a29049356aec7cf
BernardoModerno <bernardo.moderno@gmail.com>
2023-01-25 23:43:14 -0300

    Header da pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  Icon,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+} from './styles';
+
+import avatarDefault from '../../assets/avatar02.png';
 
 export const Home: React.FunctionComponent = () => {
   return (
     <Container>
-      <Title>Home Page</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Jorge</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
